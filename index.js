91a3eadc0f51a763bcccf02f7f37d782ec2750bf
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2018-12-09 13:29:57 -0800

    Added procfile and deploying to heroku

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
+const path = require('path');
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
 });
